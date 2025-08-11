5ecfcd95117c8d24d093c8cf2ee942bb5b1ff168
firdavs130828 <arabovfirdavs7@gmail.com>
2025-08-11 17:54:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// let yosh=prompt("yosh kiriting:");
+
+// if (yosh >= 8 && yosh<=17) {
+//     alert("siz mars IT schoolda da o'qishingiz mumkin");
+
+
+// } else{
+//     alert(" siz mars IT school da oqishingiz mumkin emas ");
+// }
+
+// let mashina={
+//     nomi: "malibu 2 turbo",
+//     rangi : "Qora",
+//     yili: 2025,
+//     tanirovka: true,
+//     company: "chevrolet",
+//     nomer: "01 777 UZB",
+//     probeg: 0,
+//     narxi: "45000 $",
+//     egasi:{
+//         ism:"Firdavs",
+//         familya: "Arabov",
+//         yosh: "21"
+//     }
+
+
+    
+    
+    
+// }
+// console.log(mashina.nomi);
+
+
+
+const odam={
+    name:null,
+    id:null,
+    password:null,
+};
+odam.name=prompt("ism kiriting:");
+odam.id=prompt("space id kiriting:");
+odam.password=prompt("parol kiriting");
+console.log(odam);
